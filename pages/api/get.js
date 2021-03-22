fdce355db08a3bdf53f740b6cd3dc536d16bdf6f
Chris McCauley <cdmccauley@gmail.com>
2021-03-22 11:24:42 -0600

    added log calls to api/get

--- pages/api/get.js
+++ pages/api/get.js
@@ -1,9 +1,11 @@
 export default function handler(req, res) {
   // console.log('/get POST: ', req.body.url);
+  console.log('/get: ', req.method)
 
   fetch(req.body.url)
     .then(canvasRes => canvasRes.json())
     .then(canvasData => {
+      console.log('responding')
       res.status(200).json(JSON.stringify({
         canvasData
       }))
