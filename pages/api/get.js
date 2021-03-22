e72eabdfd3cae5556873c37956f4fbdd092e14b1
Chris McCauley <cdmccauley@gmail.com>
2021-03-22 12:45:31 -0600

    updated api/get to async/await

--- pages/api/get.js
+++ pages/api/get.js
@@ -1,11 +1,11 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
   // console.log('/get POST: ', req.body.url);
-  console.log('/get: ', req.method)
+  console.log('/get: ', req.method, 'with: ', req.body.url)
 
-  fetch(req.body.url)
+  await fetch(req.body.url)
     .then(canvasRes => canvasRes.json())
     .then(canvasData => {
-      console.log('responding')
+      console.log('responding with: ', canvasData)
       res.status(200).json(JSON.stringify({
         canvasData
       }))
