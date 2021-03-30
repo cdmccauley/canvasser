cf9efaf1fc491e68deeb745005acca669e981fdb
Chris McCauley <cdmccauley@gmail.com>
2021-03-30 15:47:31 -0600

    connected to mongodb

--- pages/api/reserve.js
+++ pages/api/reserve.js
@@ -1,7 +1,24 @@
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.MONGO_CONNECTION, { useUnifiedTopology: true });
 let reserved = {}
 
 export default function handler(req, res) {
-    // console.log(process.env.MONGO_USERNAME, process.env.MONGO_PASSWORD)
+    // console.log(process.env.MONGO_CONNECTION)
+    (async ()=> {
+        try {
+            // Connect the client to the server
+            await client.connect();
+            // Establish and verify connection
+            const database = client.db(process.env.MONGO_DB);
+            const collection = database.collection(process.env.MONGO_COLLECTION)
+            console.log(collection);
+        } finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
+        }
+    })().catch(console.dir);
+
+
     if (req.method === 'POST') {
         let userId = req.body.userId.toString() // user id is stored as number in index
         let userReserved = req.body.userReserved
